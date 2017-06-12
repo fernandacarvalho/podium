d489afd7f9520976fc95ae1e8004af4671503016
fernandacarvalho <fernanda.bepid@gmail.com>
2017-06-11 23:02:06 -0300

    app js

---
+++ app/web/js/app.js
@@ -0,0 +1,63 @@
+(function () {
+
+  'use strict';
+
+  angular.module('PodiumApp',[])
+  .controller('EmailController', EmailController)
+  .service('RegisterEmailService', RegisterEmailService)
+  .constant('ApiBasePath', "http://54.207.40.40:5010/email");
+
+  EmailController.$inject = ['RegisterEmailService'];
+
+  function EmailController(RegisterEmailService){
+
+    var userEmail = this;
+
+    userEmail.registerEmail = function () {
+
+      var promise = RegisterEmailService.addEmail(userEmail.email);
+
+      promise.then(function (response) {
+      var data = response.data;
+
+      var msg = data["description"];
+      console.log(msg);
+
+      })
+      .catch(function (error) {
+        console.log("erro" + error);
+      })
+
+    };
+  }
+
+  RegisterEmailService.$inject = ['$http', 'ApiBasePath'];
+  function RegisterEmailService($http, ApiBasePath){
+
+    var service = this;
+
+    service.addEmail = function (email) {
+      console.log("email:" + email);
+      if(email != undefined){
+
+        var response = $http({
+          method: "POST",
+          url: (ApiBasePath),
+          params: {
+            email: email
+          }
+        });
+
+        return response;
+
+      }
+      else{
+        console.log("Digite email");
+      }
+
+    };
+  }
+
+
+
+})();
